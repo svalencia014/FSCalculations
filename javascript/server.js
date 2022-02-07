ab6342174e0de446e767efd3071de251bba90d3d
Samuel Valencia <68522351+svalencia014@users.noreply.github.com>
2022-02-07 11:25:41 -0500

    initial

---
+++ javascript/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const port = 8080;
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/web/'));
+});
+
+app.listen(port);
+console.log(`Server started at ${port}`);
+
---
+++ javascript/web/index.js
@@ -0,0 +1,2 @@
+a20nButton = document.querySelector("radio[name='a20n'")
+a20nButton.onClick(console.log("test"))
